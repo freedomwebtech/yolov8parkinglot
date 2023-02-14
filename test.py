066ff020f933b4437c21360c6433b19521a831e5
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-02-14 14:23:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 
 
 
-model1=YOLO('yolov8s.pt')
+model=YOLO('yolov8s.pt')
 
 
 def RGB(event, x, y, flags, param):
@@ -17,7 +17,7 @@ def RGB(event, x, y, flags, param):
 cv2.namedWindow('RGB')
 cv2.setMouseCallback('RGB', RGB)
 
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture('park.mp4')
 
 
 my_file = open("coco.txt", "r")
@@ -49,11 +49,12 @@ while True:
         x2=int(row[2])
         y2=int(row[3])
         d=int(row[5])
-#        c=class_list[d]
-        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2) 
-        cv2.putText(frame,str(d),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,0,0),1)
+        c=class_list[d]
+#        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2) 
+#        cv2.putText(frame,str(d),(x1,y1),cv2.FONT_HERSHEY_COMPLEX,0.5,(255,0,0),1)
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1)&0xFF==27:
         break
 cap.release()
 cv2.destroyAllWindows()
+
